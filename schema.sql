1d8eb0ac2ecb04f4933f228bef6a9431a47e4f01
Italo <i7alog4m3r1@gmail.com>
2026-07-30 15:49:14 -0300

    Adicionando exemplos no esquema SQL

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS agenda_contatos;
 CREATE DATABASE agenda_contatos;
 USE agenda_contatos;
 
@@ -19,7 +20,7 @@ CREATE TABLE cidades(
 CREATE TABLE contatos (
 	id_contato int PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255),
-    telefone VARCHAR(255),
+    telefone VARCHAR(15),
     id_cidade int,
     id_estado int,
     CONSTRAINT fk_contatos_cidades
@@ -96,6 +97,29 @@ UNLOCK TABLES;
 
 LOCK TABLES `contatos` WRITE;
 INSERT INTO `contatos` (`id_contato`, `nome`, `telefone`, `id_cidade`, `id_estado`)
-VALUES (3,'Carla','(11) 99999-9999',25,25),
-(5,'Dutra','(51) 99999-9999',21,21);
+VALUES (1,'Arthur','(79) 97401-3749',26,26),
+(2,'Bernardo','(11) 99999-9999',25,25),
+(3,'Carla','(51) 99999-9999',21,21),
+(4,'Dutra','(81) 99999-9999',17,17),
+(5,'Eduarda','(47) 99999-9999',24,24),
+(6,'Felipe','(61) 99999-9999',7,7),
+(7,'Gabriela','(31) 99999-9999',13,13),
+(8,'Hugo','(21) 99999-9999',19,19),
+(9,'Ingrid','(92) 99999-9999',4,4),
+(10,'João','(41) 99999-9999',16,16),
+(11,'Karina','(65) 99999-9999',11,11),
+(12,'Lucas','(83) 99999-9999',15,15),
+(13,'Maria','(69) 99999-9999',22,22),
+(14,'Nicolas','(24) 99999-9999',19,19),
+(15,'Olivia','(98) 99999-9999',10,10),
+(16,'Pedro','(91) 99999-9999',14,14),
+(17,'Quiteria','(86) 99999-9999',18,18),
+(18,'Rafael','(94) 99999-9999',14,14),
+(19,'Samara','(96) 99999-9999',3,3),
+(20,'Thiago','(27) 99999-9999',8,8),
+(21,'Vanessa','(67) 99999-9999',12,12),
+(22,'William','(84) 99999-9999',20,20),
+(23,'Ximena','(95) 99999-9999',23,23),
+(24,'Yago','(63) 99999-9999',27,27),
+(25,'Zelia','(73) 99999-9999',5,5);
 UNLOCK TABLES;
